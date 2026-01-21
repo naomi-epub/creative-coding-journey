1bcc5e694f13886db80aaf43b5c2b895cf7957ff
Naomi Nguyen <nghghakhanh@gmail.com>
2026-01-21 22:09:23 +0700

    feat: implement startup loading screen with gsap

--- tailwind-learning/src/main.js
+++ tailwind-learning/src/main.js
@@ -2,34 +2,34 @@ import './style.css'
 import gsap from 'gsap'
 
 window.addEventListener('load', () => {
-    const items = document.querySelectorAll('.bento-item,.card');
     
-    // Tạo timeline
+    const loader = document.getElementById('loader');
+    const items = document.querySelectorAll('.bento-item, .card');
+    
     const tl = gsap.timeline();
 
-    // --- HÀNH ĐỘNG 1: HIỆN NGUYÊN HÌNH (Kéo rèm) ---
-    // autoAlpha: 1 tương đương với (opacity: 1 + visibility: visible)
-    tl.to("body", { 
-        duration: 0.5, // Hiện ra nhanh trong 0.5s
-        autoAlpha: 1 
+    tl
+    .to(loader, {
+        yPercent: -100,
+        duration: 1.5,
+        ease: "power4.inOut",
     })
-    
-    // --- HÀNH ĐỘNG 2: MÚA LỬA (Bento Grid) ---
-    // Nối tiếp ngay sau khi body hiện ra
+
     .fromTo(items, 
         { 
-            y: 50, // Nini giảm xuống 50 thôi cho nó tinh tế, 100 hơi xa
+            y: 100, 
             scale: 0.9, 
-            autoAlpha: 0 // Vẫn cần ẩn lúc đầu để nó hiện dần ra
+            autoAlpha: 0 
         },
         {
             duration: 1,
             y: 0,
             scale: 1,
             autoAlpha: 1,
-            stagger: 0.1, // Nhanh hơn xíu cho năng động
+            stagger: 0.1,
             ease: "power3.out",
             force3D: true
-        }
+        },
+        "-=1.0"
     );
 });
