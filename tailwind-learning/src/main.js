617018fbe32d98c5a7c67e3fadd4499b31e23d57
Naomi Nguyen <nghghakhanh@gmail.com>
2026-01-21 17:04:39 +0700

    Feat: Add Bento Grid with Tailwind and GSAP

--- tailwind-learning/src/main.js
+++ tailwind-learning/src/main.js
@@ -1,24 +1,35 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import gsap from 'gsap'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+window.addEventListener('load', () => {
+    const items = document.querySelectorAll('.bento-item,.card');
+    
+    // Tạo timeline
+    const tl = gsap.timeline();
 
-setupCounter(document.querySelector('#counter'))
+    // --- HÀNH ĐỘNG 1: HIỆN NGUYÊN HÌNH (Kéo rèm) ---
+    // autoAlpha: 1 tương đương với (opacity: 1 + visibility: visible)
+    tl.to("body", { 
+        duration: 0.5, // Hiện ra nhanh trong 0.5s
+        autoAlpha: 1 
+    })
+    
+    // --- HÀNH ĐỘNG 2: MÚA LỬA (Bento Grid) ---
+    // Nối tiếp ngay sau khi body hiện ra
+    .fromTo(items, 
+        { 
+            y: 50, // Nini giảm xuống 50 thôi cho nó tinh tế, 100 hơi xa
+            scale: 0.9, 
+            autoAlpha: 0 // Vẫn cần ẩn lúc đầu để nó hiện dần ra
+        },
+        {
+            duration: 1,
+            y: 0,
+            scale: 1,
+            autoAlpha: 1,
+            stagger: 0.1, // Nhanh hơn xíu cho năng động
+            ease: "power3.out",
+            force3D: true
+        }
+    );
+});
