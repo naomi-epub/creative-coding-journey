32f5df6f062116a9693f5201158ae7affa16027b
Naomi Nguyen <nghghakhanh@gmail.com>
2026-01-29 11:16:22 +0700

    fix: resolve FOUC

--- projects/2-gym-landing-page/src/main.js
+++ projects/2-gym-landing-page/src/main.js
@@ -1,6 +1,10 @@
 import './style.css'
 import gsap from 'gsap'
 
+window.addEventListener('load',()=>{
+  gsap.to("body",{autoAlpha:1, duration:0.5})
+})
+
 const menuBtn = document.getElementById('menu-btn');
 const mobileMenu = document.getElementById('mobile-menu');
 const menuIcon = document.getElementById('menu-icon');
